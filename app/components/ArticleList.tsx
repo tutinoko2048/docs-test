f758ab03628a4e73b2307b82400b6e96b7fd22f4
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-09-28 00:34:08 +0900

    fix fetching url

--- app/components/ArticleList.tsx
+++ app/components/ArticleList.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import { headers } from 'next/headers'
 import { SimpleGrid, Flex, VStack, Stack, Grid } from "./chakra-ui";
 import { ArticleData } from "../types";
 import ArticleCard from "./ArticleCard";
 
 export default async function ArticleList() {
-  const res = await fetch("http://localhost:3000/api/articles", {
+  const headersData = headers();
+  const host = headersData.get('host');
+  const protocol = headersData.get('x-forwarded-proto') ?? host.startWith('localhost') ? 'http' : 'https';
+  const baseUrl = `${protocol}://${host}`;
+  
+  const res = await fetch(`${baseUrl}/api/articles`, {
     cache: "no-store"
   });
   const articles: ArticleData[] = await res.json();
