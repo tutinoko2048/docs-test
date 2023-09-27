10067023a72c427ea95ff4691042f2f45363d40c
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-09-28 00:44:58 +0900

    コピペに騙された

--- app/components/ArticleList.tsx
+++ app/components/ArticleList.tsx
@@ -6,7 +6,7 @@ import ArticleCard from "./ArticleCard";
 export default async function ArticleList() {
   const headersData = headers();
   const host = headersData.get('host');
-  const protocol = headersData.get('x-forwarded-proto') ?? host!.startWith('localhost') ? 'http' : 'https';
+  const protocol = headersData.get('x-forwarded-proto') ?? host!.startsWith('localhost') ? 'http' : 'https';
   const baseUrl = `${protocol}://${host}`;
   
   const res = await fetch(`${baseUrl}/api/articles`, {
