0a7483804b2443f47f8912a39b40491363421459
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-09-28 11:25:46 +0000

    github link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Container, Box, Text, useColorModeValue } from "./chakra-ui";
+import { Container, Box, Text, useColorModeValue, Link } from "./chakra-ui";
 
 export default function Footer() {
   const borderColor = useColorModeValue("gray.200", "gray.600");
@@ -14,6 +14,7 @@ export default function Footer() {
     >
       <Container maxW="5xl" py={4} borderTop={2} borderColor={borderColor} centerContent>
         <Text as="small">© 2023 RetoRuto9900K</Text>
+        <Link href="https://github.com/tutinoko2048/docs-test" color="blue.500" size="sm">Github</Link>
       </Container>
     </Box>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Provider from "./Provider";
+import Provider from "./components/Provider";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
