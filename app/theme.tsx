1004662515bb39aae95f34be305c12a8070de4e3
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-10-01 21:14:41 +0900

    use dark mode by default

--- app/theme.tsx
+++ app/theme.tsx
@@ -3,7 +3,7 @@ import { mode } from '@chakra-ui/theme-tools';
 
 const theme = extendTheme({
   config: {
-    initialColorMode: 'light',
+    initialColorMode: 'dark',
     useSystemColorMode: false,
   },
   colors: {
